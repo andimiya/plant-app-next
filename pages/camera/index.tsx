4faf8c7944f0eaa454157e5916efabda6880f71d
Andrea <atakamiya@classy.org>
2023-03-22 19:35:26 -0700

    Flip camera

--- pages/camera/index.tsx
+++ pages/camera/index.tsx
@@ -1,4 +1,4 @@
-import Camera, { IMAGE_TYPES } from "react-html5-camera-photo";
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 import imageUpload from "../api/upload-url";
 
@@ -14,6 +14,7 @@ export default function CameraPage() {
         onTakePhoto={(dataUri) => {
           handleTakePhoto(dataUri);
         }}
+        idealFacingMode={FACING_MODES.ENVIRONMENT}
         imageType={IMAGE_TYPES.JPG}
       />
     </div>
