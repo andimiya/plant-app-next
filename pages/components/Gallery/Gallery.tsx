d8d797be136fb41d557436e861ab304a4d8722fc
Andrea <atakamiya@classy.org>
2023-04-30 13:56:55 -0700

    Small cleanup

--- pages/components/Gallery/Gallery.tsx
+++ pages/components/Gallery/Gallery.tsx
@@ -1,7 +1,6 @@
 import { Col, Row } from "antd";
 import css from "./Gallery.module.css";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
 export default function Gallery(props: any) {
   const [data, setData] = useState<any>();
@@ -11,14 +10,13 @@ export default function Gallery(props: any) {
     }
   }, [props.images]);
 
-  console.log(data, "dataaa");
   return (
     <>
       <Row gutter={[16, 16]}>
         {data?.images ? (
           data.images.map((image: any, i: number) => (
             <Col key={i} span={8}>
-              <Image key={i} alt="image" className={css.image} src={image} />
+              <img key={i} alt="image" className={css.image} src={image} />
             </Col>
           ))
         ) : (
